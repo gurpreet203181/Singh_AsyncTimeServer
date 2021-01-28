a7aaa5eb0aa5dd054a1ff97e04f608b308e142f5
gs203181 <gs203181@gmail.com>
2021-01-28 22:03:30 +0100

    Creato wpf AsyncTimeServer e library SocetAsynLib

--- Singh_AsyncTimeServer/MainWindow.xaml.cs
+++ Singh_AsyncTimeServer/MainWindow.xaml.cs
@@ -12,17 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Singh_SocketAsyncLib;
 namespace Singh_AsyncTimeServer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary> 
     public partial class MainWindow : Window
     {
+        AsyncSocketServer mServer;
         public MainWindow()
         {
             InitializeComponent();
+            mServer = new AsyncSocketServer();
+            Icon_refersh.Visibility = Visibility.Hidden;
         }
+
+        private void btn_ascolto_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.InAscolto();
+            btn_ascolto.IsEnabled = false;
+            btn_Disconetti.IsEnabled = true;
+
+            Icon_refersh.Spin = true;
+            Icon_refersh.Visibility = Visibility.Visible;
+
+        }
+
+        private void btn_Disconetti_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.Disconnetti();
+        }
+
+        
+
     }
 }
